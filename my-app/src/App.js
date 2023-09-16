22e3d180cc00fe2561061e3939bccf75b77de747
Abhinandvb02 <abhinandvb130@gmail.com>
2023-09-16 11:24:57 +0530

    imageslider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,11 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import SideBar from './Components/SideBar';
 import Slider1 from './Components/Slider1';
-import Slider2 from './Components/Slider2';
-import Slider3 from './Components/Slider3';
 
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <SideBar />
-        <Routes>
-          <Route path='/' element={<Slider1/>}></Route>
-          <Route path='/wild' element={<Slider2/>}></Route>
-          <Route path='/cars' element={<Slider3/>}></Route>
-        </Routes>
-      </BrowserRouter>
+        <Slider1/>
     </div>
   );
 }
--- my-app/src/Components/Slider1.jsx
+++ my-app/src/Components/Slider1.jsx
@@ -1,26 +1,85 @@
-import React from 'react'
-import { Parallax } from 'react-parallax'
-import Textfile from './Textfile'
+import React, { useState } from 'react'
+import Plx from 'react-plx'
+import { Carousel } from 'react-bootstrap'
 
 function Slider1() {
+
+  const [images, SetImage] = useState([{ image: 'https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' },
+                                       { image: 'https://plus.unsplash.com/premium_photo-1675826774815-35b8a48ddc2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' },
+                                       { image: 'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' },
+                                       { image: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' },
+                                       { image: 'https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' }])
+
   return (
     <div className='maindiv'>
-      <Textfile/>
-      <Parallax className='image' bgImage='https://images.unsplash.com/photo-1548625361-58a9b86aa83b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2073&q=80'  strength={800}>
-        
-    </Parallax>
-    <Textfile/>
-    <Parallax className='image' bgImage='https://i.pinimg.com/564x/8f/aa/13/8faa13beb3bd6ba74f05ee5a65cdfd42.jpg'  strength={800}>
-        
-        </Parallax>
-        <Textfile/>
-        <Parallax className='image' bgImage='https://i.pinimg.com/736x/01/0c/0e/010c0e6122ed045e8bc8814d6ff21b12.jpg'  strength={800}>
-        
-        </Parallax>
-        <Textfile/>
-        <Parallax className='image' bgImage='https://i.pinimg.com/564x/4e/82/eb/4e82eb2db54333698e156148b7c9e93c.jpg'  strength={800}>
-        
-        </Parallax>
+      <Plx
+        parallaxData={[
+          {
+            start: 0,
+            end: 800,
+            properties: [
+              {
+                startValue: 1,
+                endValue: 1.18,
+                property: "scale"
+              }
+            ]
+          }
+        ]}
+        style={{
+          position: "relative",
+          left: 0,
+          top: 0,
+          width: "100%",
+        }}
+      >
+        <img style={{ width: "100%" }} src="https://images.unsplash.com/photo-1585409677983-0f6c41ca9c3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80" alt="background" />
+      </Plx>
+      <Plx
+        parallaxData={[
+          {
+            start: 0,
+            end: 400,
+            properties: [
+              {
+                startValue: 1,
+                endValue: 0,
+                property: "opacity"
+              }
+            ]
+          }
+        ]}
+        style={{
+          position: "fixed",
+          left: "35%",
+          top: "20vh",
+          width: "100%"
+        }}
+      >
+        <img
+          style={{
+            width: "30vw"
+          }}
+          src="https://o.remove.bg/downloads/5f168893-54d5-44b0-b85a-ca335ed5989a/360_F_233541093_muq5wycrMR9dy3rkC6uJbYb5fPprt70V-removebg-preview.png"
+          alt="explore"
+        />
+      </Plx>
+      
+      <div className='caroumain'>
+          <Carousel className='carou'>
+           {images.map((item)=>{
+             return(
+              <Carousel.Item>
+             <img
+               className="d-block w-100 carouimage"
+               src={item.image}
+               alt="Slides"
+             />
+           </Carousel.Item>
+             )
+           })}
+          </Carousel>
+        </div>
     </div>
   )
 }
